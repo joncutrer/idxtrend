b22773a03081da5f77b6d8ed6670d1be128e15bb
JC <joncutrer@gmail.com>
2022-12-07 22:18:47 -0600

    multi

---
+++ pages/About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title('📈 About IDX Trend')
+
+st.write('idxtrend is a market trend analysis dashboard.  The objective of this tool is to show market direction for trend and momtentum trading.')
