1211b2b02aa80cc6f443ef5393db71d2172de577
JC <joncutrer@gmail.com>
2022-12-07 22:25:55 -0600

    spelling

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # IDX Trend
 
 
-idxtrend is a market trend analysis dashboard.  The objective of this tool is to show market direction for trend and momtentum trading.
+idxtrend is a market trend analysis dashboard.  The objective of this tool is to show market direction for trend and momentum trading.
 
 ## Let's Go!
 
--- pages/About.py
+++ pages/About.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 st.title('📈 About IDX Trend')
 
-st.write('idxtrend is a market trend analysis dashboard.  The objective of this tool is to show market direction for trend and momtentum trading.')
+st.write('idxtrend is a market trend analysis dashboard.  The objective of this tool is to show market direction for trend and momentum trading.')
